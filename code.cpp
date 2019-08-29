1129b13361477f85746638ac4b6aeb011ff13003
nddat1908 <54591227+nddat1908@users.noreply.github.com>
2019-08-29 14:41:47 +0700

    code

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int A,B;
+	scanf("%d",&A);
+	scanf("%d",&B);
+	
+	if(A>=B){
+		printf("%d la so lon hon",A);
+	}	else{
+		printf("%d la so lon hon",B);
+	}	
+
+	return 0;
+}
+
